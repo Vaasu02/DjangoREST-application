e147b773bf1d05d351a1ad4c3d7f9eca0c736998
Vaasu02 <vasuchourasia625@gmail.com>
2025-03-19 19:50:58 +0530

    fixed-frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import { ACCESS_TOKEN } from "./constants";
 import axios from "axios";
 
+const apiUrl = "/choreo-apis/django-react/backend/v1"
+
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 })
 
 api.interceptors.request.use(
